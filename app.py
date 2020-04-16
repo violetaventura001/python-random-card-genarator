c9fb9798d33a0d359195c85c23f4b14c661163c3
Violeta Ventura <violetaventura@gmail.com>
2020-04-16 22:10:38 +0000

    randomnumber

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ card_numbers =["1","2","3","4","5","6","7","8","9","10","A","K","Q","J"]
 def get_random_card():
     return random.randint(0,13)
 
-print(card_suite[3]+""+card_numbers[4])
+print(card_numbers[get_random_card()])
+
+#+""+card_suite[get_random_card()]
