65d7307140bac3c48eaf5ae7bb72ff75945562fc
Violeta Ventura <violetaventura@gmail.com>
2020-04-17 01:36:43 +0000

    length

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import random
 
-card_suite = [" ♡ ", " ♧ ", "♤", "♢"]
-card_numbers =["1","2","3","4","5","6","7","8","9","10","A","K","Q","J"]
+card_suite = ["♡","♧","♤","♢"]
+card_numbers =["2","3","4","5","6","7","8","9","10","A","K","Q","J"]
 
-def get_random_card():
-    return random.randint(0,13)
+def get_random_card(a):
+    return random.randint(0,len(a)-1)
 
-print(card_numbers[get_random_card()])
+print(card_numbers[get_random_card(card_numbers)] +" "+ card_suite[get_random_card(card_suite)])
+#print(card_suite[get_random_card(card_suite)])
 
+#print(card_suite[-1])
 #+""+card_suite[get_random_card()]
